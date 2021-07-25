796ada3089202d6a17c4eb2ad18550a9a10b5140
Saud0227 <CarlJohanStahl0227@gmail.com>
2021-07-25 16:30:01 +0200

    begun rework of words

--- sketch.js
+++ sketch.js
@@ -1,6 +1,12 @@
 let bList=[],bN=100;
+let activeL=[];
 let fontArcade;
 let tmpW;
+
+//---------------------
+let char = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+let charBox = [];
+//---------------------
 
 function preload() {
   fontArcade = loadFont('assets/ARCADECLASSIC.TTF',function(){console.log("font loaded")},function(){console.log("font failed to load");});
@@ -14,7 +20,7 @@ function setup() {
   }
   angleMode(DEGREES);
   textFont(fontArcade);
-  tmpW=new word([0,1,2,3,4]);
+  tmpW=new words([0,1,-1,2,3,-1,4]);
 }
 
 function draw() {
@@ -25,3 +31,8 @@ function draw() {
     bList[i].draw(); 
   }
 }
+
+
+
+//Input == All chars == their index
+//space == -1
--- word.js
+++ word.js
@@ -1,15 +1,42 @@
-class word{
-
-    constructor(_x){
-        this.letters=[];
-        console.log(_x);
-        try{
-            for (let i = 0; i < _x.length; i++) {
-                this.letters[i]=new letter(_x[i],0,0);
+class words{
+    constructor(_x,){
+        this.word=[]
+        this.spaces=0;
+        //this.letters=[];
+        //try{
+        //    for (let i = 0; i < _x.length; i++) {
+        //        this.letters[i]=new letter(_x[i],0,0);
+        //    }
+        //} catch (error){
+        //    console.log("word input not an array" + error);
+        //}
+        //console.log(this.letters);
+        let tmpI=0;
+        this.word[tmpI]=[];
+        for (let i = 0; i < _x.length; i++) {
+            if(_x[i]==-1){
+                this.spaces++;
+                tmpI++;
+                this.word[tmpI]=[]
+            }else{
+                this.word[tmpI].push(_x[i]);
             }
-        } catch (error){
-            console.log(error);
+            
         }
-        console.log(this.letters);
+        console.log(this.word,this.spaces);
     }
 }
+
+
+
+//word == dived by space
+//letter == a letter (wow)
+
+
+//words asigns letters places
+//letters exit outsuide both, 
+
+
+//Hi im a test sentence
+//hi + im + a + test + sentence + 3 space
+
