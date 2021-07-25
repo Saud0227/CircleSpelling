3b432fd9dda5ce803c742f38cdab7a78e49545cf
Saud0227 <CarlJohanStahl0227@gmail.com>
2021-07-25 16:29:33 +0200

    moved charList out of class

--- letterDisp.js
+++ letterDisp.js
@@ -1,7 +1,6 @@
 class letter{
-    char = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
     constructor(_i,_x,_y){
-        this.letter=this.char[_i];
+        this.letter=char[_i];
         this.s=1;
         this.pos=createVector(_x,_y);
         this.deg=0;
